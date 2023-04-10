5d5b259380aa9d0c05636ce7eeedbf7096cffcc8
Nikita Samsonenkov <mrkimatsuki@gmail.com>
2023-04-10 17:56:22 +0500

    Commit #2

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
-# This is a sample Python script.
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, EmailField, SelectField
+from wtforms.validators import DataRequired, Optional
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class ExampleForm(FlaskForm):
+    name = StringField('Имя', validators=[DataRequired(message="Поле не должно быть пустым")])
+    text = TextAreaField('Отзыв', validators=[DataRequired(message="Поле не должно быть пустым")])
+    email = EmailField('Email', validators=[Optional()])
+    rating = SelectField('Оценка', choices=[1, 2, 3, 4, 5])
+    submit = SubmitField('Отправить')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+context = '<h1>Заголовок!</h1>'
+
+
+@app.route('/')
+def start_page():
+    return render_template("index.html")
+
+
+@app.route('/test/<int:n>')
+def test(n):
+    return render_template("test.html", context=context)
+
+
+@app.route('/news')
+def new():
+    return "Новости"
+
+
+@app.route('/example')
+def example():
+    return render_template("example_test.html")
+
+
+@app.route('/news_detail/<int:id>')
+def news_detail(id):
+    return render_template("news_detail.html")
+
+
+@app.route('/category/<string:name>')
+def category(name):
+    return f"Категория: {name}"
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = ExampleForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        text = form.text.data
+        email = form.email.data
+        rating = form.rating.data
+        print(name, text, email, rating)
+        return redirect('/')
+    return render_template("form.html", form=form)
